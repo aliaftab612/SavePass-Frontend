5e3a607d1a339cd9220fa5e0994494da83dbbcd8
Aftab Ali <aliaftab612@gmail.com>
2024-10-31 11:36:53 +0530

    changed prod env variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  serverBaseUrl: 'https://savepass.live',
+  serverBaseUrl: 'https://api.savepass.live',
   passwordlessDevPublicAPIKey:
     'savepass:public:58bac7aa37834a7e883edc5e1cf40756',
-  passwordlessDevPublicAPIUrl: 'https://savepass.live/api/v1',
+  passwordlessDevPublicAPIUrl: 'https://api.savepass.live/api/v1',
 };
